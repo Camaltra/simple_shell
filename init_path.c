cbebae5902fde90929506c2fae2f5a9ed5557a2d
Camalta <camaltra38@gmail.com>
2021-12-08 13:08:13 +0000

    Reverse

--- init_path.c
+++ init_path.c
@@ -8,10 +8,12 @@
 void init_path(void)
 {
 	char *path_value;
-	int i = 0, j, k, length, checkpoint = 0;
+	int i = 0, j, k, length;
 	char *value_to_fill;
+	int checkpoint = 0;
 
 	path_head = NULL;
+
 	if ((path_value = _getenv("PATH")))
 	{
 		for (i = 0; path_value[i]; i++)
@@ -36,30 +38,5 @@ void init_path(void)
 				checkpoint = i;
 			}
 		}
-		add_last_path_value(path_value);
 	}
-}
-
-
-void add_last_path_value(char *path_value)
-{
-	int i, j;
-	char *value_to_fill;
-	
-	for (i = _strlen(path_value) - 1, j = 0; path_value[i] != ':'; i--, j++)
-		;
-	value_to_fill = malloc(sizeof(char) * j + 2);
-	if (!value_to_fill)
-		return;
-	i++;
-	for (j = 0 ; path_value[i]; i++, j++)
-		value_to_fill[j] = path_value[i];
-	if (value_to_fill[j - 1] != '/')
-	{
-		value_to_fill[j] = '/';
-		value_to_fill[j + 1] = '\0';
-	}
-	else
-		value_to_fill[j] = '\0';
-	add_node_end(&path_head, value_to_fill);
 }
